eec9db7246715a40a07d94d77bbbd6809b8fa3c9
JulioRob23 <123261341+JulioRob23@users.noreply.github.com>
2023-05-04 17:04:58 -0600

    Add files via upload

---
+++ Semana 10 Organización del código/Ejercicio Cafetera.cs
@@ -0,0 +1,70 @@
+﻿using System;
+class Cafetera
+{
+    int codigo;
+    int capacidad, disponibles;
+    bool lleno;
+    // hacer cafe
+    private void hacerCafe ()
+    {
+        lleno = true;
+        disponibles = capacidad; 
+    }
+
+    //servrir taza
+    public void servirTaza (int cantTazas)
+    {
+        if ( disponibles >= cantTazas)
+        {
+            disponibles = disponibles - cantTazas;
+        }
+        else
+        {
+            Console.WriteLine("No tenemos suficientes tazas disponibles");
+        }
+    }
+
+    //obtener porcenteaje de disponibilidad 
+    public double obtenerPorcentaje()
+    {
+        double porcentaje = ((double)disponibles / capacidad) * 100; 
+        return porcentaje;
+    }
+
+    //mostrar estado 
+    public void mostrarEstado()
+    {
+        string texto = codigo + " capacidad:" + capacidad + "Tazas servida: " + (capacidad - disponibles) +
+            "porcentaje: " + obtenerPorcentaje() + "%"; 
+        Console.WriteLine(texto);
+    }
+    // constructor 
+    public Cafetera(int elCodigo, int laCapacidad)
+    {
+        codigo = elCodigo;
+        capacidad = laCapacidad;
+        lleno = false;
+        disponibles = 0;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese capacidad de cafetera");
+        int cap = Convert.ToInt32(Console.ReadLine());
+        string opcion = "";
+        Cafetera cafe = new Cafetera(1,cap);
+        
+        cafe.hacerCafe();
+        do
+        {
+            Console.WriteLine("Cuantas tazas quiere servir?");
+            int tz = Convert.ToInt32(Console.ReadLine());
+
+            cafe.servirTaza(tz);
+            cafe.mostrarEstado();
+
+            Console.WriteLine("Desea ingresar mas tazas? s/n");
+            opcion = Console.ReadLine().ToLower();
+        } while (opcion == "s");
+        
+    }
+}
---
+++ Semana 11 Organización del código (2)/Program (1).cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class Libro
+{
+    int correlativo;
+    string nombre;
+    int Nopags, total;
+    double porcentajeleido; 
+    bool leido;
+
+    public void LeerLibro()
+    {
+        leido = false;
+        total = Nopags; 
+
+    }
+
+    public void LeercantPaginas(int cantidadpags)
+    {
+        if(total >= cantidadpags)
+        {
+            total = total - cantidadpags;
+        }
+        else
+        {
+            Console.WriteLine("Error: el libro no tiene tantas páginas");
+        }
+    }
+
+    public double ObtenerPorcentaje()
+    {
+        double porcentaje = ((double)total / Nopags) * 100; 
+        porcentajeleido = porcentaje;
+        return porcentaje;
+    } 
+
+    public int PaginaActual()
+    {
+        int pagac = total;  
+        return pagac;
+    }
+    public void Obtenerestado()
+    {
+        if (porcentajeleido == 100)
+        {
+            leido = true;
+        }
+        else
+        {
+            leido = false;
+        }
+    }
+    public void MostrarInfo()
+    {
+        string texto = nombre + "  Páginas por leer: " + total + " Páginas leídas " + (Nopags - total) + " Porcentaje por leer: " + ObtenerPorcentaje() + "%";
+        Console.WriteLine(texto);
+    }
+    public Libro (int elTotal, string elNombre)
+    {
+        Nopags = elTotal;
+        leido = false; 
+        nombre = elNombre;
+
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese el nombre del libro");
+        string nombrelibro = Console.ReadLine();
+        Console.WriteLine("Ingrese el número de páginas que contiene el libro");
+        int cap = Convert.ToInt32(Console.ReadLine());
+        
+        string seguir = "";
+        Libro libros = new Libro(cap,nombrelibro);
+
+        libros.LeerLibro();
+        do
+        {
+            Console.WriteLine("Cuantas páginas desea leer");
+            int pal = Convert.ToInt32(Console.ReadLine());
+
+            libros.LeercantPaginas(pal);
+            libros.MostrarInfo();
+
+            Console.WriteLine("Desea leer más páginas? s/n");
+            seguir = Console.ReadLine().ToLower();
+        } while (seguir == "s");
+
+    }
+}
